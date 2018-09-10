033cbf092ac9940a101e15a8bc1d011d34482c9d
vliang <vliang@mac-mini-4.intranet.theknot.com>
2018-09-10 17:18:59 +0800

    add mobile header

--- src/components/head/head.js
+++ src/components/head/head.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
 import './head.css';
+import '../../share.css';
 
 export default class Head extends Component {
+  constructor () {
+    super();
+    this.state = {
+      showMenu: false
+    }
+    this.toggleShowMenu = this.toggleShowMenu.bind(this);
+  }
+
+  toggleShowMenu() {
+    this.setState(state => ({
+      showMenu: !state.showMenu,
+    }));
+  }
+
   render() {
+    const { showMenu } = this.state;
+
     return(
-      <header>
-        <div>hello 宠回家</div>
+      <header className='text-white background-lightblue'>
+        <div className='head-title text-center'>hello 宠回家</div>
         <nav>
-          <ul>
-            <li>首页</li>
-            <li>领养信息</li>
-            <li>领养要求</li>
-            <li>关于我们</li>
-          </ul>
+          <div className='menu-icon menu-btn background-white' onClick={ this.toggleShowMenu }></div>
+          { showMenu ?
+            (
+              <ul className='menu-container display-flex flex-wrap background-lightblue'>
+                <li className='menu-item'>首页</li>
+                <li className='menu-item'>领养信息</li>
+                <li className='menu-item'>领养要求</li>
+                <li className='menu-item'>关于我们</li>
+              </ul>
+            ) : null
+          }
         </nav>
       </header>
     )
