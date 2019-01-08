ed3e62f65d5b5f71a6229008eafe1c8ce877fbc0
sgemini <s13268001833@gmail.com>
2019-01-08 23:29:51 +0800

    add search icon and popup container

--- src/components/head/head.js
+++ src/components/head/head.js
@@ -1,48 +1,93 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './head.css';
 import '../../app.css';
 import '../../config.js';
+
+import Popup from '../popup/popup.js';
 
 export default class Head extends Component {
   constructor () {
     super();
     this.state = {
-      showMenu: false
+      showMenu: false,
+      isPopup: false,
+      showSearchPopup: false,
     }
-    this.toggleShowMenu = this.toggleShowMenu.bind(this);
   }
 
-  toggleShowMenu() {
+  static defaultProps = {
+    headTitle: 'hello 宠回家',
+    showSearch: true,
+  }
+
+  toggleMenu = () => {
     this.setState(state => ({
       showMenu: !state.showMenu,
     }));
   }
 
-  render() {
+  closePopup = () => {
+    this.setState({
+      isPopup: false,
+      showSearchPopup: false,
+    })
+  }
+
+  showSearchPopup = () => {
+    this.setState({
+      showSearchPopup: true,
+      isPopup: true,
+    })
+  }
+
+  headMenu = () => {
+    const { mobile } = global.browser.versions;
     const { showMenu } = this.state;
-    const { mobile } = global.browser.versions
 
     return(
-      <header>
-        <div className='header--title'>hello 宠回家</div>
-        <nav>
-          <div className='menu-icon mobile-only header--nav---btn' onClick={ this.toggleShowMenu }></div>
-          <ul className='header--menu---list' style={{ display: !mobile || showMenu ? 'flex' : 'none' }}>
-            <li className='header--menu---item'>
-              <a href='/'>首页</a>
-            </li>
-            <li className='header--menu---item'>
-              <a href='/animals'>领养信息</a>
-            </li>
-            <li className='header--menu---item'>
-              <a href='/adoption-tips'>领养要求</a>
-            </li>
-            <li className='header--menu---item'>
-              <a href='/about'>关于我们</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
+      <nav>
+        <div className='menu-icon mobile-only header--nav---btn' onClick={ this.toggleMenu }></div>
+        <ul className='header--menu---list' style={{ display: !mobile || showMenu ? 'flex' : 'none' }}>
+          <li className='header--menu---item'>
+            <a href='/'>首页</a>
+          </li>
+          <li className='header--menu---item'>
+            <a href='/animals'>领养信息</a>
+          </li>
+          <li className='header--menu---item'>
+            <a href='/adoption-tips'>领养要求</a>
+          </li>
+          <li className='header--menu---item'>
+            <a href='/about'>关于我们</a>
+          </li>
+        </ul>
+      </nav>
+    )
+  }
+
+  popupCloseBtn = () => {
+    return(
+      <nav>
+        <div className='close-btn mobile-only header--close-btn' onClick={ this.closePopup }>X</div>
+      </nav>
+    )
+  }
+
+  render() {
+    const { isPopup, showSearchPopup } = this.state;    
+    const { headTitle, showSearch } = this.props;
+
+    return(
+      <Fragment>
+        <header>
+          <div className='search-icon mobile-only header--search-container' onClick={ this.showSearchPopup }>
+            <span className='search-icon header--search-container---search-icon'></span>
+          </div>
+          <div className='header--title'>{ headTitle }</div>
+          { isPopup ? this.popupCloseBtn() : this.headMenu() }
+        </header>
+        { showSearchPopup ? <Popup /> : null }
+      </Fragment>
     )
   }
 }
